3a1d48db47ed3784e531e31f36d409b758e3b899
Programming <meexiong.2018@gmail.com>
2018-05-12 14:23:56 +0700

    1. Update New Doctor frame 	- RadioButton in 	- Jtable 2. Update Main frame TaskPane

--- src/com/mlk/views/FrmDoctorList.java
+++ src/com/mlk/views/FrmDoctorList.java
@@ -163,7 +163,7 @@ public class FrmDoctorList extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnNewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewActionPerformed
-        frmNewDoctor newDoctorObject = new frmNewDoctor(null, closable);
+        FrmNewDoctor newDoctorObject = new FrmNewDoctor(null, closable);
         newDoctorObject.setVisible(true);
     }//GEN-LAST:event_btnNewActionPerformed
 
